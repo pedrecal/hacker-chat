5ddcd5375193f546356cdb2151be67e25c9a1726
Alexandre Pedrecal <pedrecalc@gmail.com>
2021-03-22 21:05:36 -0300

    add: #SemanaJSExpert Day1

---
+++ client/src/constants.js
@@ -0,0 +1,14 @@
+const constants = {
+  events: {
+    app: {
+      MESSAGE_SENT: "message:sent",
+      MESSAGE_RECEIVED: "message:received",
+      ACTIVITYLOG_UPDATED: "activityLog:updated",
+      STATUS_UPDATED: "status:updated",
+    },
+
+    socket: {},
+  },
+};
+
+export { constants };
---
+++ client/src/terminalController.js
@@ -0,0 +1,99 @@
+import ComponentsBuilder from "./components.js";
+import { constants } from "./constants.js";
+
+class TerminalController {
+  #usersColors = new Map();
+
+  constructor() {}
+
+  #pickColor() {
+    return `#${(((1 << 24) * Math.random()) | 0).toString(16)}-fg`;
+  }
+
+  #getUserColor(userName) {
+    if (this.#usersColors.has(userName)) {
+      return this.#usersColors.get(userName);
+    }
+
+    const color = this.#pickColor();
+    this.#usersColors.set(userName, color);
+
+    return color;
+  }
+
+  #onInputReceived(eventEmitter) {
+    return function () {
+      const message = this.getValue();
+      console.log(message);
+      this.clearValue();
+    };
+  }
+
+  #onMessageReceived({ screen, chat }) {
+    return (msg) => {
+      const { userName, message } = msg;
+      const color = this.#getUserColor(userName);
+
+      chat.addItem(`{${color}}{bold}${userName}{/}: ${message}`);
+      screen.render();
+    };
+  }
+
+  #onLogChanged({ screen, activityLog }) {
+    return (msg) => {
+      const [userName] = msg.split(/\s/);
+      const color = this.#getUserColor(userName);
+
+      activityLog.addItem(`{${color}}{bold}${msg.toString()}{/}`);
+
+      screen.render();
+    };
+  }
+
+  #onStatusChanged({ screen, status }) {
+    return (users) => {
+      const { content } = status.items.shift();
+      status.clearItems();
+      status.addItem(content);
+
+      users.forEach((userName) => {
+        const color = this.#getUserColor(userName);
+        status.addItem(`{${color}}{bold}${userName}{/}`);
+      });
+      screen.render();
+    };
+  }
+
+  #registerEvents(eventEmitter, components) {
+    eventEmitter.on(
+      constants.events.app.MESSAGE_RECEIVED,
+      this.#onMessageReceived(components)
+    );
+    eventEmitter.on(
+      constants.events.app.ACTIVITYLOG_UPDATED,
+      this.#onLogChanged(components)
+    );
+    eventEmitter.on(
+      constants.events.app.STATUS_UPDATED,
+      this.#onStatusChanged(components)
+    );
+  }
+
+  async initializeTable(eventEmitter) {
+    const components = new ComponentsBuilder()
+      .setScreen({ title: "4DevsChat" })
+      .setLayoutComponent()
+      .setInputComponent(this.#onInputReceived(eventEmitter))
+      .setChatComponent()
+      .setActivityLogComponent()
+      .setStatusComponent()
+      .build();
+
+    this.#registerEvents(eventEmitter, components);
+
+    components.input.focus();
+    components.screen.render();
+  }
+}
+
+export default TerminalController;
